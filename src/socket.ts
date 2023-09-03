ec93f03c4cd056b4bc17684ad51c6b4c1a398fcb
TheEasyLemon <dawsonren@gmail.com>
2023-09-03 13:46:36 -0500

    update favicon and add in environment variables

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,12 @@
 
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001'); // Change URL as needed
+let socket
+
+if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_SERVER_URL !== undefined) {
+    socket = io(process.env.REACT_APP_SERVER_URL)
+} else {
+    socket = io('http://localhost:3001')
+}
 
 export default socket;
